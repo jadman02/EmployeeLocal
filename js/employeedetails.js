5107c8a15175fa34f98d456b7531208b7ffa51fb
jadman02 <jadman02@hotmail.com>
2014-09-07 13:47:05 +1000

    ss

--- js/employeedetails.js
+++ js/employeedetails.js
@@ -1,6 +1,6 @@
-var serviceURL = localStorage['serviceURL'];
-var scroll = new iScroll('wrapper', { vScrollbar: false, hScrollbar:false, hScroll: false });
+var serviceURL = "http://smilesavers.net.au/";
 
+var employees;
 var id = getUrlVars()["id"];
 
 $(window).load(function() {
